4e8b7763e31b6780633de6abcf9c22f280f349ac
vincentARM <51370770+vincentARM@users.noreply.github.com>
2019-11-07 14:56:02 +0100

    Update ficmacros64.s

--- ficmacros64.s
+++ ficmacros64.s
@@ -21,7 +21,7 @@
     b smacroafficheMess\@   // pour sauter le stockage de la chaine.
 libaff1\@:  .ascii "\str"
                .asciz "\n"
-.align 8
+.align 4
 smacroafficheMess\@:
 .endm                       // fin de la macro
 /********************************************************************/
@@ -38,7 +38,7 @@ smacroafficheMess\@:
     ldr x1,[sp],32          // on restaure x1 pour avoir une pile réalignée
     b smacro1affbintit\@    // pour sauter le stockage de la chaine.
 libbin1\@:  .asciz "\str"
-.align 8
+.align 4
 smacro1affbintit\@:
 .endm
 /********************************************************************/
@@ -59,7 +59,7 @@ smacro1affbintit\@:
     ldp x0,x1,[sp],32          // on restaure x1 pour avoir une pile réalignée
     b smacro1affregtit\@    // pour sauter le stockage de la chaine.
 libreg1\@:  .asciz "\str"
-.align 8
+.align 4
 smacro1affregtit\@:
 .endm
 /********************************************************************/
@@ -76,16 +76,16 @@ smacro1affregtit\@:
     ldr x1,[sp],32          // on restaure x1 pour avoir une pile réalignée
     b smacro1affetattit\@    // pour sauter le stockage de la chaine.
 libetat1\@:  .asciz "\str"
-.align 8
+.align 4
 smacro1affetattit\@:
 .endm
 /****************************************************/
 /* macro de vidage memoire                          */
 /****************************************************/
-/* n'affiche que les adresses ou les registre r0 et r1      */
+/* n'affiche que les adresses ou les registre x0 et x1      */
 .macro affmemtit str, adr, nb 
-    stp x0,x1,[sp,-32]!        // save  registre
-    stp x2,x3,[sp,16]          // save  registre
+    stp x0,x1,[sp,-32]!        // save  registres
+    stp x2,x3,[sp,16]          // save  registres
     mrs x3,nzcv                // save du registre d'état dans x3
     adr x2,lib1\@              // recup adresse libellé passé dans str
     .ifc \adr,x1
@@ -98,8 +98,8 @@ smacro1affetattit\@:
     mov x1,#\nb                // nombre de bloc a afficher
     bl affmemoireTit
     msr nzcv,x3                // restaur registre d'état
-    ldp x0,x1,[sp],16          // restaur des registre
-    ldp x2,x3,[sp],16          // restaur des registr
+    ldp x0,x1,[sp],16          // restaur des registres
+    ldp x2,x3,[sp],16          // restaur des registres
     b smacro1affmemtit\@       // pour sauter le stockage de la chaine.
 .ifnc \adr,x0
 .ifnc \adr,x1
